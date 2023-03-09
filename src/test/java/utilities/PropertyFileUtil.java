b087fb2d4f5686f2880e43334d43dee84dcc75d4
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-03-09 20:12:03 +0530

    First Commits

--- src/test/java/utilities/PropertyFileUtil.java
+++ src/test/java/utilities/PropertyFileUtil.java
@@ -1,5 +1,13 @@
 package utilities;
 
-public class PropertyFileUtil {
+import java.io.FileInputStream;
+import java.util.Properties;
 
+public class PropertyFileUtil {
+public static String getValueForKey(String key)throws Throwable
+{
+	Properties conprop = new Properties();
+	conprop.load(new FileInputStream("./PropertyFiles/Environment.properties"));
+	return conprop.getProperty(key);
+}
 }
