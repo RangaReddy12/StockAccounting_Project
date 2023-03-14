e8dddb9bc51fdd6d67b21b44865f5be8dd4ec306
Lenovo <Lenovo@DESKTOP-3557JUS>
2023-03-14 20:15:02 +0530

    march 13th

--- src/test/java/utilities/ExcelFileUtil.java
+++ src/test/java/utilities/ExcelFileUtil.java
@@ -1,5 +1,103 @@
 package utilities;
 
-public class ExcelFileUtil {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelFileUtil {
+	Workbook wb;
+
+	//constructor for reading excel path
+	public ExcelFileUtil(String Excelpath)throws Throwable
+	{
+		FileInputStream fi = new FileInputStream(Excelpath);
+		wb = WorkbookFactory.create(fi);		
+	}
+
+	// count no of rows in a sheet
+	public int rowCount(String Sheetname)
+	{
+		return wb.getSheet(Sheetname).getLastRowNum();
+	}
+
+	//method for get cell data
+	public String getCellData(String sheetname,int row,int column)
+	{
+		String data = "";
+		if (wb.getSheet(sheetname).getRow(row).getCell(column).getCellType()==Cell.CELL_TYPE_NUMERIC)
+		{
+			int celldata=(int) wb.getSheet(sheetname).getRow(row).getCell(column).getNumericCellValue(); 
+			data = String.valueOf(celldata);
+		}
+		else
+		{
+			data = wb.getSheet(sheetname).getRow(row).getCell(column).getStringCellValue();			
+		}
+		return data;
+	}
+
+	//method for setCell
+	public void setCellData(String shettname,int row ,int column,String Status,String wrtiteExcel)throws Throwable
+	{
+		//get sheet from wb
+		Sheet ws = wb.getSheet(shettname);
+		//get roe from sheet
+		Row rownum = ws.getRow(row);
+		//create cell in row
+		Cell cell = rownum.createCell(column);
+		//write cell
+		cell.setCellValue(Status);
+		if(Status.equalsIgnoreCase("Pass"))
+		{
+			CellStyle style = wb.createCellStyle();
+			Font font = wb.createFont();
+			font.setColor(IndexedColors.GREEN.getIndex());
+			font.setBold(true);
+			font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+			style.setFont(font);
+			rownum.getCell(column).setCellStyle(style);
+		}
+		else if(Status.equalsIgnoreCase("Fail"))
+		{
+			CellStyle style = wb.createCellStyle();
+			Font font = wb.createFont();
+			font.setColor(IndexedColors.RED.getIndex());
+			font.setBold(true);
+			font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+			style.setFont(font);
+			rownum.getCell(column).setCellStyle(style);
+		}
+		else if(Status.equalsIgnoreCase("Block"))
+		{
+			CellStyle style = wb.createCellStyle();
+			Font font = wb.createFont();
+			font.setColor(IndexedColors.BLUE.getIndex());
+			font.setBold(true);
+			font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+			style.setFont(font);
+			rownum.getCell(column).setCellStyle(style);
+		}
+		FileOutputStream fo = new FileOutputStream(wrtiteExcel);
+		wb.write(fo);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/utilities/PropertyFileUtil.java
+++ src/test/java/utilities/PropertyFileUtil.java
@@ -1,5 +1,14 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class PropertyFileUtil {
 
+	public static String getValueForKey(String key)throws Throwable
+	{
+		Properties conpro = new Properties();
+		conpro.load(new FileInputStream("./PropertyFiles//Environment.propertie"));
+		return conpro.getProperty(key);
+	}
 }
