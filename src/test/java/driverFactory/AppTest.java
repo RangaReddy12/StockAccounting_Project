9fbe77e8fdecfb8ec244ae5e7196535206564c94
God-Father04 <god04father@gmail.com>
2023-03-15 18:51:52 +0530

    aman project

--- src/test/java/driverFactory/AppTest.java
+++ src/test/java/driverFactory/AppTest.java
@@ -1,5 +1,15 @@
 package driverFactory;
 
+import org.testng.annotations.Test;
+
 public class AppTest {
+@Test
+
+public void kickStart() throws Throwable
+{
+	DriverScript ds= new DriverScript();
+	ds.startTest();
+}
+
 
 }
--- src/test/java/driverFactory/DriverScript.java
+++ src/test/java/driverFactory/DriverScript.java
@@ -1,5 +1,173 @@
 package driverFactory;
 
+import org.openqa.selenium.WebDriver;
+
+import commonFunctions.FunctionLibrary;
+import utilities.ExcelFileUtil;
+
 public class DriverScript {
+
+	public static WebDriver driver;
+
+	String inputpath ="F:\\OJT\\StockAccounting_Project\\FileInput\\DataEngine.xlsx";
+
+	String outputpath ="F:\\OJT\\StockAccounting_Project\\FileOutput\\HybridResults.xlsx";
+
+	public void startTest()throws Throwable
+	{
+
+		String ModuleStatus="";
+
+		//create Object for excel FileUtil class
+
+		ExcelFileUtil xl = new ExcelFileUtil(inputpath);
+
+		//iterate all rows in MasterTestcases sheet
+
+		for(int i=1;i<=xl.rowCount("MasterTestCases");i++)
+
+		{
+
+			//read execution status cell
+
+			String Execution_Status = xl.getCellData("MasterTestCases", i, 2);
+
+			if(Execution_Status.equalsIgnoreCase("Y"))
+
+			{
+
+				//read corresponding sheet from MasterTestCases
+
+				String TCModule =xl.getCellData("MasterTestCases", i, 1);
+
+				//iterate corresponding sheet
+
+				for(int j=1;j<=xl.rowCount(TCModule);j++)
+
+				{
+
+					//read all cells from TCModule sheet
+
+					String Description =xl.getCellData(TCModule, j, 0);
+
+					String ObjectType =xl.getCellData(TCModule, j, 1);
+
+					String LocatorType =xl.getCellData(TCModule, j, 2);
+
+					String LocatorValue =xl.getCellData(TCModule, j, 3);
+
+					String TestData = xl.getCellData(TCModule, j, 4);
+
+					try {
+
+						if(ObjectType.equalsIgnoreCase("startBrowser"))
+
+						{
+
+							driver = FunctionLibrary.startBrowser();
+
+						}
+
+						else if(ObjectType.equalsIgnoreCase("openUrl"))
+
+						{
+
+							FunctionLibrary.openUrl(driver);
+
+						}
+
+						else if(ObjectType.equalsIgnoreCase("waitForElement"))
+
+						{
+
+							FunctionLibrary.waitForElement(driver, LocatorType, LocatorValue, TestData);
+
+						}
+
+						else if(ObjectType.equalsIgnoreCase("typeAction"))
+
+						{
+
+							FunctionLibrary.typeAction(driver, LocatorType, LocatorValue, TestData);
+
+						}
+
+						else if(ObjectType.equalsIgnoreCase("clickAction"))
+
+						{
+
+							FunctionLibrary.clickAction(driver, LocatorType, LocatorValue);
+
+						}
+
+						else if(ObjectType.equalsIgnoreCase("validateTitle"))
+
+						{
+
+							FunctionLibrary.validateTitle(driver, TestData);
+
+						}
+
+						else if(ObjectType.equalsIgnoreCase("closeBrowser"))
+
+						{
+
+							FunctionLibrary.closeBrowser(driver);
+
+						}
+
+						//write as pass into TCModule sheet
+
+						xl.setCellData(TCModule, j, 5, "Pass", outputpath);
+
+						ModuleStatus="True";
+
+					}catch(Exception e)
+
+					{
+
+						System.out.println(e.getMessage());
+
+						//write as Fail into TCModule sheet
+
+						xl.setCellData(TCModule, j, 5, "Fail", outputpath);
+
+						ModuleStatus ="False";
+
+					}
+
+					if(ModuleStatus.equalsIgnoreCase("True"))
+
+					{
+
+						xl.setCellData("MasterTestCases", i, 3, "Pass", outputpath);
+
+					}
+
+					else
+
+					{
+
+						xl.setCellData("MasterTestCases", i, 3, "Fail", outputpath);
+
+					}
+
+				}
+
+			}
+
+			else
+
+			{
+
+				//write as blocked which test case flag to N
+
+				xl.setCellData("MasterTestCases", i, 3, "Blocked", outputpath);
+
+			}
+
+		}
+
+	}
 
 }
