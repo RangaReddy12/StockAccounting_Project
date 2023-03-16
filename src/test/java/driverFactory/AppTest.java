87f53c77be40a84b4ec43f039a12fd1c31d19ce9
KulkarniSharda <127321797+KulkarniSharda@users.noreply.github.com>
2023-03-16 18:04:11 +0530

    Revert "Sharda"

--- src/test/java/driverFactory/AppTest.java
+++ src/test/java/driverFactory/AppTest.java
@@ -2,8 +2,4 @@ package driverFactory;
 
 public class AppTest {
 
-	
-		
-		
-	
 }
--- src/test/java/utilities/PropertyFileUtil.java
+++ src/test/java/utilities/PropertyFileUtil.java
@@ -4,17 +4,10 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 public class PropertyFileUtil {
-	
 public static String getValueForKey(String key)throws Throwable
 {
-	
 	Properties conprop = new Properties();
-	//conprop.load(new FileInputStream("./PropertyFiles/Environment.properties"));
-	conprop.load(new FileInputStream("C:\\Users\\PASKAL\\git\\StockAccounting_Project\\PropertyFiles\\Environment.properties"));
-
-	
+	conprop.load(new FileInputStream("./PropertyFiles/Environment.properties"));
 	return conprop.getProperty(key);
-	
-	
 }
 }
